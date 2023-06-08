68d30f395fa50d8d3a10f1872006a613e14b6ed5
Thiago <thiagoalgusto_2025@ghotmail.com.br>
2023-06-08 14:36:06 -0300

    hello world, usando wire com apex

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,9 +1,13 @@
-import { LightningElement, api, wire, track } from 'lwc'; // aqui você está importanto a funcionalidade básica do lwc
+import { LightningElement } from 'lwc'; // aqui você está importanto a funcionalidade básica do lwc
 // caso for usar precisa-se importar aqui em cima
 
 export default class HelloWorld extends LightningElement {
-    firstName = 'hello world !!'; // você não precisa tipar um tipo pois como é JS ele já tipa para você o tipo pelo tipo de dado inserido, mas diferente do js aqui você não precisa dar um var ou let para criar uma variavel
- 
+    nome = 'Thiago'; // você não precisa tipar um tipo pois como é JS ele já tipa para você o tipo pelo tipo de dado inserido, mas diferente do js aqui você não precisa dar um var ou let para criar uma variavel
+
+    atualizar(element) {
+        this.nome = element.target.value;
+    }
+
     /**
      *  por padrão as variaveis criadas sem nomeclatura são privadas,
      *  caso você quiser criar uma propriedade publica você coloca @api
@@ -13,6 +17,7 @@ export default class HelloWorld extends LightningElement {
      *  privada => variavel;
      *  rederiza um componente quando ocorre uma alteração => @track
      *  pega e mescla um dado da org
-     *  @wire
+     *  @wire(adapterId, {adapterConfig})
+     *  variavel ou função;
      * */  
 }
--- force-app/main/default/lwc/wireApex/wireApex.js
+++ force-app/main/default/lwc/wireApex/wireApex.js
@@ -5,6 +5,6 @@ export default class WireApex extends LightningElement {
 
     @api recordId;
     
-    @wire(getContatos, {idConta: '$recordId'})
+    @wire(getContatos, {idConta: '$recordId'}) // @wire(pego o método getContatos, { aqui eu passo a informação que irá receber por parametro que no caso o idConta e quem recebe ela é o $recordId | $ <-(informar que é uma variavel)})
     contatos;
 }
